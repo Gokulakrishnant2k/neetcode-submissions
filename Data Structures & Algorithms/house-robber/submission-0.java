dd2cd8edb4edf39cfb596054a5e725dbdf97e9b5
Gokulakrishnant2k <115548480+Gokulakrishnant2k@users.noreply.github.com>
2026-05-01 23:04:40 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+    int prev1 = 0; 
+    int prev2 = 0; 
+
+    for (int num : nums) {
+        int take = num + prev2;   
+        int skip = prev1;        
+
+        int curr = Math.max(take, skip);
+
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+    }
+}
